b0687f5d58182aded7c7496d88c1a1bef3145e1d
Mayuri <mayuriumaretiya111@gmail.com>
2023-07-08 23:40:44 +0530

    initial commit

---
+++ note.js
@@ -0,0 +1,47 @@
+// for(i=1;i<=10;i++){
+//     console.log(i**2)
+// }
+
+// let item = ['bred','banana','chocalate','$20'];
+// console.log(item.slice(0,item.length));
+
+// for(i=0;i<item.length;i++){
+//     console.log(item[i]);
+
+// }
+
+// const Mayuri = {name : 'MAYURI',age : 18}
+// console.log(`Before :${Mayuri.name}`)
+// console.log(Mayuri.age)
+// Mayuri.name = 'MAy'
+// console.log(`After :${Mayuri.name}`)
+
+// console.log(Mayuri['name'])
+
+// let arr = ['apple','banana','cat','dog',50];
+// for(element of arr){
+//     console.log(element)
+// }
+
+// for(element in arr){
+//     console.log(element)
+// }
+
+// let obj = {}
+// let phrase = 'Hello My Name is Mayuri'
+// let arr = phrase.split(' ')
+
+// for(word of phrase){
+//     console.log(word)
+//     if(word in obj){
+//         obj[word]++
+//     }else{
+//         obj[word] = 1
+//     }
+// }
+
+// console.log(obj)
+
+ 
+
+
